64556b597e49251dbf9089bb7b7c1b876c15f6bd
Sorwar Ahmed <sorwar@Sorwars-MacBook-Pro.local>
2022-02-27 06:06:46 +0000

    heroku-err

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,19 +5,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', '') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/LineIcons.2.0.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/LineIcons.2.0.css') }}">
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     </head>
     <body class="font-sans antialiased bg-white dark:bg-slate-900">
