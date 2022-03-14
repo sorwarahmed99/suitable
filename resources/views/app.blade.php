d31d55fefcbcb402e29ae4d57024623c0548e770
Sorwar Ahmed <sorwar@Sorwars-MacBook-Pro.local>
2022-03-14 21:47:16 +0000

    Cors Package added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        
+
         <title inertia>{{ config('app.name', '') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
